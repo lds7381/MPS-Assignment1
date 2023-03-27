b6a319f0e9a0300f27378b3b2e4898113b239895
Atticus Russell <60147768+atticusrussell@users.noreply.github.com>
2023-03-27 14:14:05 -0400

    Merge bugfix into main (#2)
    * added y sending func
    
    * fix and y changes
    
    * fixes
    
    * bracket fix
    
    * added more protection for rank 0
    
    * num tasks check and each task should be printing that they are starting... none are
    
    * fixed OOM error, getting incorrect task syncronization
    
    * fixed int to double and made dendrites calculation be non master
    
    * moved mpi finalize
    
    * added inital send of y values
    
    * should be working
    
    * fixed memory free issue, getting repeat values, might not be correct values being recorded
    
    * More cleanup
    
    * ran autoformatter - now legible
    
    * fixed autoformat glitch
    
    * fix soma param issue, should be fully working
    
    * Bash script for q3 and q 4
    
    * fixed core amt
    
    * small core change
    
    ---------
    
    Co-authored-by: Liam David Sullivan <lds7381@sporcsubmit.rc.rit.edu>
    Co-authored-by: Liam Sullivan <lds7381@rit.edy>

---
+++ project/questions_3_4.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#
+
+# This is an example bash script that is used to submit a job
+# to the cluster.
+#
+# Typcially, the # represents a comment.However, #SBATCH is
+# interpreted by SLURM to give an option from above. As you 
+# will see in the following lines, it is very useful to provide
+# that information here, rather than the command line.
+
+# Name of the job - You MUST use a unique name for the job
+#SBATCH -J p6d15c10
+
+# Standard out and Standard Error output files
+# Each job should have a unique file name; otherwise, all of the
+# output goes to one file and becomes hard to read/analyze.
+# The default file name looks like <job name>-<job ID>.out.
+#SBATCH -o %x-%j.out
+#SBATCH -e %x-%j.err
+
+# In order for this to send emails, you will need to remove the
+# space between # and SBATCH for the following 2 commands.
+# Specify the recipient of the email
+# SBATCH --mail-user=abc1234@rit.edu
+
+# Notify on state change: BEGIN, END, FAIL or ALL
+# SBATCH --mail-type=ALL
+
+# Multiple options can be used on the same line as shown below.
+# Here, we set the partition and the number of cores to use,
+# and specify the amount of memory we would like per core.
+#SBATCH -p kgcoe-mps -n 11
+#SBATCH --mem-per-cpu 1G
+
+#
+# Your job script goes below this line.
+#
+
+# If the job that you are submitting is not sequential,
+# then you MUST provide this line...it tells the node(s)
+# that you want to use this implementation of MPI. If you
+# omit this line, your results will indicate failure.
+spack load --first openmpi
+
+# Place your srun command here
+# Notice that you have to provide the number of processes that
+# are needed. This number needs to match the number of cores
+# indicated by the -n option. If these do not, your results will
+# not be valid or you may have wasted resources that others could
+# have used. Using $SLURM_NPROCS guarantees a match.
+
+# Question Set 3
+srun -n 11 mpi_hh -d 30 -c 100
+srun -n 11 mpi_hh -d 33 -c 100
+srun -n 11 mpi_hh -d 36 -c 100
+
+# Question Set 4 
+srun -n 6 mpi_hh -d 5 -c 1000
+srun -n 6 mpi_hh -d 6 -c 1000
+srun -n 6 mpi_hh -d 7 -c 1000
+srun -n 6 mpi_hh -d 1499 -c 10
+srun -n 6 mpi_hh -d 1500 -c 10
+srun -n 6 mpi_hh -d 1501 -c 10
--- project/src/mpi_hh.c
+++ project/src/mpi_hh.c
@@ -6,17 +6,17 @@
   This is a Hodgkin Huxley (HH) simplified compartamental neuron model
 */
 
-#include "plot.h"
 #include "mpi_hh.h"
 #include "cmd_args.h"
 #include "constants.h"
+#include "plot.h"
 
-#include <time.h>
+#include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <sys/time.h>
-#include <mpi.h>
+#include <time.h>
 
 // Define macros based on compilation options. This is a best practice that
 // ensures that all code is seen by the compiler so there will be no surprises
@@ -43,102 +43,138 @@
  * Parameters:
  * @param argc    number of command line arguments
  * @param argv    command line arguments
-*/
-int main( int argc, char **argv )
-{
+ */
+int main(int argc, char **argv) {
+  CmdArgs cmd_args;                        // Command line arguments.
+  int num_comps, num_dendrs, r_num_dendrs; // Simulation parameters.
+  int i, j, t_ms, step, dendrite;          // Various indexing variables.
+  struct timeval start, stop, diff;        // Values used to measure time.
 
-  CmdArgs cmd_args;                       // Command line arguments.
-  int num_comps, num_dendrs, r_num_dendrs;              // Simulation parameters.
-  int i, j, t_ms, step, dendrite;         // Various indexing variables.
-  struct timeval start, stop, diff;       // Values used to measure time.
-
-  double exec_time;  // How long we take.
+  double exec_time; // How long we take.
 
   // Accumulators used during dendrite simulation.
   // NOTE: We depend on the compiler to handle the use of double[] variables as
   //       double*.
   double current, **dendr_volt;
   double res[COMPTIME], y[NUMVAR], y0[NUMVAR], dydt[NUMVAR], soma_params[3];
+  double temp_soma[3];
 
   // Strings used to store filenames for the graph and data files.
   char time_str[14];
-  char graph_fname[ FNAME_LEN ];
-  char data_fname[ FNAME_LEN ];
+  char graph_fname[FNAME_LEN];
+  char data_fname[FNAME_LEN];
 
   FILE *data_file;  // The output file where we store the soma potential values.
   FILE *graph_file; // File where graph will be saved.
 
-  PlotInfo pinfo;   // Info passed to the plotting functions.
-
+  PlotInfo pinfo; // Info passed to the plotting functions.
 
   //////////////////////////////////////////////////////////////////////////////
-  // Parse command line arguments.  (DONT EDIT)
+  // Initalize MPI  (ADDED IN - EDIT HERE)
   //////////////////////////////////////////////////////////////////////////////
 
-  if (!parseArgs( &cmd_args, argc, argv )) {
-	// Something was wrong.
-	exit(1);
+  /* ********** MPI Variables ********** */
+  int numtasks, rank, rc, receives = 0;
+  int n; // indexing
+  MPI_Status status;
+
+  // Initalize MPI
+  rc = MPI_Init(&argc, &argv);
+  // Check if successful
+  if (rc != MPI_SUCCESS) {
+    fprintf(stderr, "Error starting MPI.\n");
+    MPI_Abort(MPI_COMM_WORLD, rc);
+  }
+
+  // Get rank and number of tasks
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+
+  // Make sure we have atleas the minimum amount of tasks needed (6)
+  if (numtasks < 6) {
+    if (rank == 0) {
+      printf("Unable to start necessary amount of MPI tasks!\n");
+      MPI_Abort(MPI_COMM_WORLD, rc);
+      return 0;
+    }
+  } else {
+    if (rank > 0) {
+      printf("Task %d Starting... \n", rank);
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Parse command line arguments.
+  //////////////////////////////////////////////////////////////////////////////
+
+  if (!parseArgs(&cmd_args, argc, argv)) {
+    // Something was wrong.
+    exit(1);
   }
 
   // Pull out the parameters so we don't need to type 'cmd_args.' all the time.
   num_dendrs = cmd_args.num_dendrs;
-  num_comps  = cmd_args.num_comps;
+  num_comps = cmd_args.num_comps;
 
-  printf( "Simulating %d dendrites with %d compartments per dendrite.\n",
-		  num_dendrs, num_comps );
-
-  //////////////////////////////////////////////////////////////////////////////
-  // Create files where results will be stored. (DONT EDIT)
-  //////////////////////////////////////////////////////////////////////////////
-
-  // Generate the graph and data file names.
-  time_t t = time(NULL);
-  struct tm *tmp = localtime( &t );
-  strftime( time_str, 14, "%m%d%y_%H%M%S", tmp );
-
-  // The resulting filenames will resemble
-  //    pWWdXXcYY_MoDaYe_HoMiSe.xxx
-  // where 'WW' is the number of processes, 'XX' is the number of dendrites,
-  // 'YY' the number of compartments, and 'MoDaYe...' the time at which this
-  // simulation was run.
-  sprintf( graph_fname, "graphs/p1d%dc%d_%s.png",
-		   num_dendrs, num_comps, time_str );
-  sprintf( data_fname,  "data/p1d%dc%d_%s.dat",
-		   num_dendrs, num_comps, time_str );
-
-  // Verify that the graphs/ and data/ directories exist. Create them if they
-  // don't.
-  struct stat stat_buf;
-  stat( "graphs", &stat_buf );
-  if ((!S_ISDIR(stat_buf.st_mode)) && (mkdir( "graphs", 0700 ) != 0)) {
-	fprintf( stderr, "Could not create 'graphs' directory!\n" );
-	exit(1);
-  }
-
-  stat( "data", &stat_buf );
-  if ((!S_ISDIR(stat_buf.st_mode)) && (mkdir( "data", 0700 ) != 0)) {
-	fprintf( stderr, "Could not create 'data' directory!\n" );
-	exit(1);
-  }
-
-  // Verify that we can open files where results will be stored.
-  if ((data_file = fopen(data_fname, "wb")) == NULL) {
-	fprintf(stderr, "Can't open %s file!\n", data_fname);
-	exit(1);
-  } else {
-	printf( "\nData will be stored in %s\n", data_fname );
-  }
-
-  if (ISDEF_PLOT_PNG && (graph_file = fopen(graph_fname, "wb")) == NULL) {
-	fprintf(stderr, "Can't open %s file!\n", graph_fname);
-	exit(1);
-  } else {
-	printf( "Graph will be stored in %s\n", graph_fname );
-	fclose(graph_file);
+  if (rank == 0) {
+    printf("Simulating %d dendrites with %d compartments per dendrite.\n",
+           num_dendrs, num_comps);
   }
 
   //////////////////////////////////////////////////////////////////////////////
-  // Initialize simulation parameters. (DONT EDIT)
+  // Create files where results will be stored.
+  //////////////////////////////////////////////////////////////////////////////
+
+  if (rank == 0) {
+    // Generate the graph and data file names.
+    time_t t = time(NULL);
+    struct tm *tmp = localtime(&t);
+    strftime(time_str, 14, "%m%d%y_%H%M%S", tmp);
+
+    // The resulting filenames will resemble
+    //    pWWdXXcYY_MoDaYe_HoMiSe.xxx
+    // where 'WW' is the number of processes, 'XX' is the number of dendrites,
+    // 'YY' the number of compartments, and 'MoDaYe...' the time at which this
+    // simulation was run.
+    sprintf(graph_fname, "graphs/p1d%dc%d_%s.png", num_dendrs, num_comps,
+            time_str);
+    sprintf(data_fname, "data/p1d%dc%d_%s.dat", num_dendrs, num_comps,
+            time_str);
+
+    // Verify that the graphs/ and data/ directories exist. Create them if they
+    // don't.
+    struct stat stat_buf;
+    stat("graphs", &stat_buf);
+    if ((!S_ISDIR(stat_buf.st_mode)) && (mkdir("graphs", 0700) != 0)) {
+      fprintf(stderr, "Could not create 'graphs' directory!\n");
+      exit(1);
+    }
+
+    stat("data", &stat_buf);
+    if ((!S_ISDIR(stat_buf.st_mode)) && (mkdir("data", 0700) != 0)) {
+      fprintf(stderr, "Could not create 'data' directory!\n");
+      exit(1);
+    }
+
+    // Verify that we can open files where results will be stored.
+    if ((data_file = fopen(data_fname, "wb")) == NULL) {
+      fprintf(stderr, "Can't open %s file!\n", data_fname);
+      exit(1);
+    } else {
+      printf("\nData will be stored in %s\n", data_fname);
+    }
+
+    if (ISDEF_PLOT_PNG && (graph_file = fopen(graph_fname, "wb")) == NULL) {
+      fprintf(stderr, "Can't open %s file!\n", graph_fname);
+      exit(1);
+    } else {
+      printf("Graph will be stored in %s\n", graph_fname);
+      fclose(graph_file);
+    }
+  }
+
+  //////////////////////////////////////////////////////////////////////////////
+  // Initialize simulation parameters.
   //////////////////////////////////////////////////////////////////////////////
 
   // The first compartment is a dummy and the last is connected to the soma.
@@ -151,70 +187,60 @@ int main( int argc, char **argv )
   y[3] = 0.9959;
 
   // Setup parameters for the soma.
-  soma_params[0] = 1.0 / (double) STEPS;  // dt
-  soma_params[1] = 0.0;  // Direct current injection into soma is always zero.
-  soma_params[2] = 0.0;  // Dendritic current injected into soma. This is the
-						 // value that our simulation will update at each step.
+  soma_params[0] = 1.0 / (double)STEPS; // dt
+  soma_params[1] = 0.0; // Direct current injection into soma is always zero.
+  soma_params[2] = 0.0; // Dendritic current injected into soma. This is the
+                        // value that our simulation will update at each step.
 
-  printf( "\nIntegration step dt = %f\n", soma_params[0]);
-
-  // Start the clock.
-  gettimeofday( &start, NULL );
-
-  // Initialize the potential of each dendrite compartment to the rest voltage.
-  dendr_volt = (double**) malloc( num_dendrs * sizeof(double*) );
-  for (i = 0; i < num_dendrs; i++) {
-	dendr_volt[i] = (double*) malloc( num_comps * sizeof(double) );
-	for (j = 0; j < num_comps; j++) {
-	  dendr_volt[i][j] = VREST;
-	}
-  }
-
-  //////////////////////////////////////////////////////////////////////////////
-  // Initalize MPI  (ADDED IN - EDIT HERE)
-  //////////////////////////////////////////////////////////////////////////////
-
-  /* ********** MPI Variables ********** */
-  int numtasks, rank, rc, receives = 0, param;
-  int n; // indexing
-  MPI_Status status;
-
-  // Initalize MPI 
-  rc = MPI_Init( &argc, &argv );
-  // Check if successful
-  if (rc != MPI_SUCCESS) {
-    fprintf( stderr, "Error starting MPI.\n" );
-    MPI_Abort( MPI_COMM_WORLD, rc );
-  }
-
-  // Get rank and number of tasks
-  MPI_Comm_rank( MPI_COMM_WORLD, &rank);
-  MPI_Comm_size( MPI_COMM_WORLD, &numtasks);
-
-  // IF master node
   if (rank == 0) {
-    printf("Starting...  Number of Tasks: %d\n", numtasks);
+    printf("\nIntegration step dt = %f\n", soma_params[0]);
+
+    // Start the clock.
+    gettimeofday(&start, NULL);
+  }
+
+  if (rank != 0) {
+
+    // Initialize the potential of each dendrite compartment to the rest
+    // voltage.
+    dendr_volt = (double **)malloc(num_dendrs * sizeof(double *));
+    for (i = 0; i < num_dendrs; i++) {
+      dendr_volt[i] = (double *)malloc(num_comps * sizeof(double));
+      for (j = 0; j < num_comps; j++) {
+        dendr_volt[i][j] = VREST;
+      }
+    }
   }
 
   //////////////////////////////////////////////////////////////////////////////
-  // Main computation.  (EDIT HERE)
+  // Main computation.
   //////////////////////////////////////////////////////////////////////////////
 
-  // Record the initial potential value in our results array. #1
-  res[0] = y[0];
-
-  // Update num dendrites to be split among tasks
-  r_num_dendrs = num_dendrs % (numtasks - 1);
-  num_dendrs = num_dendrs / (numtasks - 1);
-  // Handle remainder
-  if (r_num_dendrs > 0) {
-    int cur_task = 1;
-    for (n = 0; n < r_num_dendrs; n++) {
-      if (cur_task > numtasks) {cur_task = 1;}
-      if (rank == cur_task) {
-        num_dendrs++;
+  if (rank > 0) {
+    // Update num dendrites to be split among tasks
+    r_num_dendrs = num_dendrs % (numtasks - 1);
+    num_dendrs = num_dendrs / (numtasks - 1);
+    // Handle remainder
+    if (r_num_dendrs > 0) {
+      int cur_task = 1;
+      for (n = 0; n < r_num_dendrs; n++) {
+        if (cur_task > numtasks) {
+          cur_task = 1;
+        }
+        if (rank == cur_task) {
+          num_dendrs++;
+        }
+        cur_task++;
       }
-      cur_task++;
+    }
+  }
+
+  if (rank == 0) {
+    // Record the initial potential value in our results array. #1
+    res[0] = y[0];
+    // send initalial starting y values
+    for (int i = 1; i <= (numtasks - 1); i++) {
+      MPI_Send(y, 4, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
     }
   }
 
@@ -229,33 +255,41 @@ int main( int argc, char **argv )
 
       // Loop over all the dendrites. #3 (Start MPI Break up here)
       if (rank != 0) {
+
+        // receive previous y values (if past first step)
+        MPI_Recv(y, 4, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
         for (dendrite = 0; dendrite < num_dendrs; dendrite++) {
-          // This will update Vm in all compartments and will give a new injected
-          // current value from last compartment into the soma.
-          current = dendriteStep( dendr_volt[ dendrite ],
-                      step + dendrite + 1,
-                      num_comps,
-                      soma_params[0],
-                      y[0] );
+          // This will update Vm in all compartments and will give a new
+          // injected current value from last compartment into the soma.
+          current = dendriteStep(dendr_volt[dendrite], step + dendrite + 1,
+                                 num_comps, soma_params[0], y[0]);
           // Accumulate the current generated by the dendrite.
           soma_params[2] += current;
         }
 
         // Send Soma Params and y to SOMA Master
-        MPI_Send( &(soma_params[2]), 1, MPI_INT, 0, 1, MPI_COMM_WORLD );
+        MPI_Send(soma_params, 3, MPI_DOUBLE, 0, 1, MPI_COMM_WORLD);
 
-      }
-      else {
-
+      } else {
+        receives = 0;
+        temp_soma[0] = 0.0;
+        temp_soma[1] = 0.0;
+        temp_soma[2] = 0.0;
         // Receive Soma Params (BLOCKING UNTIL RECIEVE FROM EACH SOURCE)
-        while (receives != (numtasks - 1)){
-          MPI_Recv( &param, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status );
-          soma_params[2] += param;
+        while (receives != (numtasks - 1)) {
+          MPI_Recv(temp_soma, 3, MPI_DOUBLE, MPI_ANY_SOURCE, MPI_ANY_TAG,
+                   MPI_COMM_WORLD, &status);
+          soma_params[2] += temp_soma[2];
+          temp_soma[2] = 0.0;
           receives++;
         }
 
         // Store previous HH model parameters.
-        y0[0] = y[0]; y0[1] = y[1]; y0[2] = y[2]; y0[3] = y[3];
+        y0[0] = y[0];
+        y0[1] = y[1];
+        y0[2] = y[2];
+        y0[3] = y[3];
 
         // ********* SOMA *********
 
@@ -264,69 +298,82 @@ int main( int argc, char **argv )
         soma(dydt, y, soma_params);
         rk4Step(y, y0, dydt, NUMVAR, soma_params, 1, soma);
 
+        // Send updated y value to all
+        for (int i = 1; i <= (numtasks - 1); i++) {
+          MPI_Send(y, 4, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
+        }
+      }
     }
 
-    // Record the membrane potential of the soma at this simulation step.
-    // Let's show where we are in terms of computation.
-    printf("\r%02d ms",t_ms); fflush(stdout);
+    if (rank == 0) {
+      // Record the membrane potential of the soma at this simulation step.
+      // Let's show where we are in terms of computation.
+      printf("\r%02d ms, res = %f", t_ms, y[0]);
+      fflush(stdout);
 
-    res[t_ms] = y[0];
+      res[t_ms] = y[0];
+    }
   }
 
-  // CLOSE MPI
-  MPI_Finalize();
-
   //////////////////////////////////////////////////////////////////////////////
-  // Report results of computation. (DONT EDIT)
+  // Report results of computation.
   //////////////////////////////////////////////////////////////////////////////
+  if (rank == 0) {
+    // Stop the clock, compute how long the program was running and report that
+    // time.
+    gettimeofday(&stop, NULL);
+    timersub(&stop, &start, &diff);
+    exec_time = (double)(diff.tv_sec) + (double)(diff.tv_usec) * 0.000001;
+    printf("\n\nExecution time: %f seconds.\n", exec_time);
 
-  // Stop the clock, compute how long the program was running and report that
-  // time.
-  gettimeofday( &stop, NULL );
-  timersub( &stop, &start, &diff );
-  exec_time = (double) (diff.tv_sec) + (double) (diff.tv_usec) * 0.000001;
-  printf("\n\nExecution time: %f seconds.\n", exec_time);
+    // Record the parameters for this simulation as well as data for gnuplot.
+    fprintf(data_file,
+            "# Vm for HH model. "
+            "Simulation time: %d ms, Integration step: %f ms, "
+            "Compartments: %d, Dendrites: %d, Execution time: %f s, "
+            "Slave processes: %d\n",
+            COMPTIME, soma_params[0], num_comps - 2, num_dendrs, exec_time,
+            numtasks);
+    fprintf(data_file, "# X Y\n");
 
-  // Record the parameters for this simulation as well as data for gnuplot.
-  fprintf( data_file,
-		   "# Vm for HH model. "
-		   "Simulation time: %d ms, Integration step: %f ms, "
-		   "Compartments: %d, Dendrites: %d, Execution time: %f s, "
-		   "Slave processes: %d\n",
-		   COMPTIME, soma_params[0], num_comps - 2, num_dendrs, exec_time,
-		   0 );
-  fprintf( data_file, "# X Y\n");
+    for (t_ms = 0; t_ms < COMPTIME; t_ms++) {
+      fprintf(data_file, "%d %f\n", t_ms, res[t_ms]);
+    }
 
-  for (t_ms = 0; t_ms < COMPTIME; t_ms++) {
-	fprintf(data_file, "%d %f\n", t_ms, res[t_ms]);
+    fflush(data_file); // Flush and close the data file so that gnuplot will
+    fclose(data_file); // see it.
+
+    //////////////////////////////////////////////////////////////////////////////
+    // Plot results if approriate macro was defined
+    //////////////////////////////////////////////////////////////////////////////
+    if (ISDEF_PLOT_PNG || ISDEF_PLOT_SCREEN) {
+      pinfo.sim_time = COMPTIME;
+      pinfo.int_step = soma_params[0];
+      pinfo.num_comps = num_comps - 2;
+      pinfo.num_dendrs = num_dendrs;
+      pinfo.exec_time = exec_time;
+      pinfo.slaves = 0;
+    }
+
+    if (ISDEF_PLOT_PNG) {
+      plotData(&pinfo, data_fname, graph_fname);
+    }
+    
+    if (ISDEF_PLOT_SCREEN) {
+      plotData(&pinfo, data_fname, NULL);
+    }
   }
-  fflush(data_file);  // Flush and close the data file so that gnuplot will
-  fclose(data_file);  // see it.
 
   //////////////////////////////////////////////////////////////////////////////
-  // Plot results if approriate macro was defined. (DONT EDIT)
-  //////////////////////////////////////////////////////////////////////////////
-  if (ISDEF_PLOT_PNG || ISDEF_PLOT_SCREEN) {
-	pinfo.sim_time = COMPTIME;
-	pinfo.int_step = soma_params[0];
-	pinfo.num_comps = num_comps - 2;
-	pinfo.num_dendrs = num_dendrs;
-	pinfo.exec_time = exec_time;
-	pinfo.slaves = 0;
-  }
-
-  if (ISDEF_PLOT_PNG) {    plotData( &pinfo, data_fname, graph_fname ); }
-  if (ISDEF_PLOT_SCREEN) { plotData( &pinfo, data_fname, NULL ); }
-
-  //////////////////////////////////////////////////////////////////////////////
-  // Free up allocated memory. (DONT EDIT)
+  // Free up allocated memory.
   //////////////////////////////////////////////////////////////////////////////
 
-  for(i = 0; i < num_dendrs; i++) {
-	free(dendr_volt[i]);
+  for (i = 0; i < num_dendrs; i++) {
+    free(dendr_volt[i]);
   }
   free(dendr_volt);
 
+  // CLOSE MPI
+  MPI_Finalize();
   return 0;
-}
 }
